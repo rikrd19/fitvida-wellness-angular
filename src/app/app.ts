f7fab72d09658ddd95a39e32730bb500ca6b1d0e
Ricardo Avila <ricardoavilabazalar@gmail.com>
2026-03-26 18:19:53 +0100

    feat: implementacion de servicios, formularios de contacto y footer corporativo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Necesario para el *ngFor
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-// IMPORTA EL NUEVO COMPONENTE AQUÍ:
-import { HeroCard} from './components/hero-card/hero-card';
+import { MatCardModule } from '@angular/material/card'; // Nuevo
+import { MatFormFieldModule } from '@angular/material/form-field'; // Nuevo
+import { MatInputModule } from '@angular/material/input'; // Nuevo
+import { HeroCard } from './components/hero-card/hero-card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  // AÑÁDELO A LA LISTA DE IMPORTS:
-  imports: [MatToolbarModule, MatButtonModule, HeroCard],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    HeroCard
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class AppComponent {
-  title = 'fitvida-app';
+  // Lista de servicios para las tarjetas
+  services = [
+    { title: 'Yoga', desc: 'Encuentra tu equilibrio interior.', price: '40€' },
+    { title: 'Pilates', desc: 'Fortalece tu core y postura.', price: '45€' },
+    { title: 'CrossFit', desc: 'Lleva tu energía al límite.', price: '50€' }
+  ];
 }
